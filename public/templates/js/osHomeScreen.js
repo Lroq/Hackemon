cf9bef80e01b3935e8df204c466ca207adff8ea6
hdylan <hebrard.dylan@ynov.com>
2026-05-12 20:35:02 +0200

    ADD SECURITY /build v9.2

--- public/templates/js/osHomeScreen.js
+++ public/templates/js/osHomeScreen.js
@@ -599,7 +599,7 @@ function openHackEngine(event) {
   }
 
   // Dépose le token en cookie avant la navigation
-  document.cookie = `auth_token=${accessToken}; path=/; SameSite=Lax; Secure`;
+  document.cookie = `auth_token=${sessionStorage.getItem('auth_token')}; path=/; SameSite=Lax; Secure`;
 
   window.location.href = '/build';
 }
